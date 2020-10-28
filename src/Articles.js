475bcb2ca84fb775855a21c765465e6ccbf5fbbb
Jaroslav Landa <JayLNX97@gmail.com>
2020-10-28 20:20:30 +0100

    fixed articles router

--- src/Articles.js
+++ src/Articles.js
@@ -1,49 +1,25 @@
 import React from "react";
 import "./styles/style.css";
-import articlesData from "./data/articlesData.json";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import ArticlesNav from "./ArticlesNav";
 import Club from "./Club";
 import ChomutovVeVesmiru from "./articles_content/ChomutovVeVesmiru";
 
 function Articles() {
-  const articleLinks = articlesData;
-
   return (
     <div className="content">
       <h1>Články</h1>
-      <nav>
-        <Router>
-          <ul className="articles-bar">
-            {articleLinks.map((link) => (
-              <Link to={link.path} key={link.id}>
-                <li className="article-link">
-                  <div className="article-preview">
-                    <img
-                      className="picture"
-                      alt="náhled článku"
-                      src={link.picture}
-                    />
-                    <div className="info">
-                      <div className="title">{link.title}</div>
-                      <div className="preview-text">{link.previewText}</div>
-                    </div>
-                  </div>
-                </li>
-              </Link>
-            ))}
-          </ul>
-          <Switch>
-            <Route
-              exact path="/articles/chomutov-ve-vesmiru"
-              component={ChomutovVeVesmiru}
-            />
-            <Route
-              path="/articles/projekt-hvezdarna-f-j-gerstnera"
-              component={Club}
-            />
-          </Switch>
-        </Router>
-      </nav>
+      <Switch>
+        <Route exact path="/articles" component={ArticlesNav} />
+        <Route
+          path="/articles/chomutov-ve-vesmiru"
+          component={ChomutovVeVesmiru}
+        />
+        <Route
+          path="/articles/projekt-hvezdarna-f-j-gerstnera"
+          component={Club}
+        />
+      </Switch>
     </div>
   );
 }
---
+++ src/ArticlesNav.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./styles/style.css";
+import articlesData from "./data/articlesData.json";
+import { Link } from "react-router-dom";
+
+function ArticlesNav() {
+  const articleLinks = articlesData;
+
+  return (
+    <nav>
+      <ul className="articles-bar">
+        {articleLinks.map((link) => (
+          <Link to={link.path} key={link.id}>
+            <li className="article-link">
+              <div className="article-preview">
+                <img
+                  className="picture"
+                  alt="náhled článku"
+                  src={link.picture}
+                />
+                <div className="info">
+                  <div className="title">{link.title}</div>
+                  <div className="preview-text">{link.previewText}</div>
+                </div>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export default ArticlesNav;
