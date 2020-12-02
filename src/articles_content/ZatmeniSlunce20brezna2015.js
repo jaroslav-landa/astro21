0ca882ff30d99a5cea372b49f9c065e86ec10170
Jaroslav Landa <JayLNX97@gmail.com>
2020-12-02 11:17:21 +0100

    Add article about solar eclipse

---
+++ src/articles_content/ZatmeniSlunce20brezna2015.js
@@ -0,0 +1,143 @@
+import React from "react";
+import "../styles/style.css";
+
+function ZatmeniSlunce20brezna2015() {
+  return (
+    <div>
+      <h2>Zatmění slunce 20. března 2015</h2>
+      <p>
+        Zatmění Slunce je jeden z nejkrásnějších přírodních úkazů, který člověk
+        může za život spatřit. Tato příležitost se nám po několika letech opět
+        naskytla v pátek 20. března 2015. Tentokrát jsme připravili první
+        pozorování částečného zatmění Slunce, na které jsme pozvali i veřejnost.
+      </p>
+      <p>
+        Úkaz je tak zajímavý, že z řady zemí vyjíždějí expedice, až do velmi
+        vzdálených míst, odkud mohou pozorovat úplné zatmění Slunce. Úplná
+        zatmění Slunce nejsou viditelná vždy ze stejného místa, nýbrž jsou
+        viditelná z různých částí naší planety. Pás kam dopadá stín Měsíce na
+        zemském povrchu je velmi úzký, kdežto jeho délka dosáhne několika tisíc
+        km. Mnohdy stín prochází nepřístupnými, neobydlenými místy a často
+        prochází nad hladinou moří a světových oceánů.
+      </p>
+      <img
+        title="částečné zatmění slunce 31.5.2013"
+        alt="částečné zatmění slunce"
+        src={require("../images/2003-5-31-chomutov--jl.jpg")}
+      />
+      <p>
+        Vědecký přínos, který taková výprava slibuje, je tak veliký, že ani
+        nebezpečí špatného počasí a daleké cesty astronoma neodradí. Podle Dr
+        Huberta Slouky (Pohledy do nebe, Praha 1949) první česká astronomická
+        expedice za zatměním Slunce se vydala na svoji cestu v roce 1936 do
+        Japonska. A druhá expedice současně zamířila do Sovětského svazu. Obě
+        výpravy byly velmi úspěšné. Pás stínu na zemském povrchu tehdy měřil na
+        šířku 150km a na délku asi 14 000 km.
+      </p>
+      <p>
+        Na pozorování částečného zatmění Slunce v Chomutově jsme byli připraveni
+        několik týdnů předem. Informace o naší nabídce veřejného pozorování
+        zatmění Slunce byla od 1. března 2015 zveřejněna na stránkách{" "}
+        <a
+          class="ext-link"
+          href="https://www.astro.cz/na-obloze/slunce/zatmeni-slunce/zatmeni-slunce-v-letech-2011-2020/zatmeni-slunce-20-brezna-2015/akce-v-ceske-republice.html#chomutov"
+        >
+          České astronomické společnosti.
+        </a>
+      </p>
+      <img
+        title="Česká astronomická společnost"
+        alt="logo české astronomické společnosti"
+        src={require("../images/banner-cas.png")}
+      />
+      <p>
+        Místnímu tisku a internetovým serverům jsme včas rozeslali pozvánky a
+        základní informace. Je škoda, že informaci zveřejnila jen dvě média
+        (Chomutovský deník a mujChomutov.cz). Díky Chomutovskému deníku se den
+        předem lidé dozvěděli o připravované akci v našem městě.
+      </p>
+      <p>
+        Na stránkách serveru mujChomutov.cz byla pozvánka zveřejněna řadu dní
+        předem. Oběma jmenovaným médiím mockrát děkujeme. ZŠ Duhová cesta s.r.o
+        stihla část místních obyvatel pozvat letáčkem vloženým do schránek v
+        okolních ulicích.
+      </p>
+      <p>
+        Poslední dny před zatměním, byly vyplněny toliko starostmi o počasí v
+        den zatmění. Neodvážili jsme se hlasitě pronést přání, aby vyšlo počasí
+        a pozorování se podařilo. Pro všechny případy jsme připravili i náhradní
+        program.
+      </p>
+      <p>
+        20. března 2015 jsme díky skvělému počasí byli svědky velmi zajímavého
+        přírodního úkazu. Naše první společné pozorování určené i pro veřejnost
+        se dle názoru přítomných opravdu podařilo. Děkujeme!
+      </p>
+      <p>
+        Pěkné snímky najdete ve{" "}
+        <a
+          class="ext-link"
+          href="http://www.zsduhovacesta.cz/fotogalerie/zatmeni"
+        >
+          fotogalerii ZŠ a MŠ Duhová cesta s.r.o
+        </a>{" "}
+        Foto: Josef Griml
+      </p>
+      <p>
+        Naše první veřejné společné pozorování 20. března 2015 nám díky počasí
+        vyšlo na výbornou. Děkujeme Vám všem za upřímná slova a poděkování.
+        Můžeme Vás ujistit, že máme připravené další zajímavé akce,
+        prostřednictvím kterých Vás chceme pozvat do nádherného vesmíru, jehož
+        jsme nedílnou součástí.
+      </p>
+      <p>
+        Naše město bylo poprvé uvedeno na stránkách České astronomické
+        společnosti jako místo, kde je nabídnut astronomický program nejširší
+        veřejnosti. Toho si velmi vážíme a uvědomujeme si, že nás to do budoucna
+        zavazuje. Počítáme s Vaší pozorností a zájmem o vesmír, kosmonautiku,
+        Zemi a přírodu. Bez toho by naše dobrovolná práce neměla smysl.
+      </p>
+      <p>
+        <br />
+        Kroužek astronomie a zš Duhová cesta s.r.o Chomutov děkuje:
+        <br /> Všem spolužákům, kteří nám pomáhali s přípravou společného
+        pozorování
+        <br /> Děkujeme České astronomické společnosti za podporu
+        <br />
+        Děkujeme redakci Chomutovského deníku za zveřejnění Pozvánky na
+        pozorování <br />
+        Děkujeme serveru mujChomutov.cz za zveřejnění Pozvánky na pozorování
+        <br />
+        Děkujeme serveru týdeníku Nástup za zveřejnění textu a fotogalerie
+        <br />
+        Děkujeme astronomickému serveru APO za zveřejnění článku o pozorvání
+        <br />
+        Děkujeme všem, kteří nám poslali fotografie z pozorování zatmění Slunce
+        <br />
+        Děkujeme všem, kteří se přišli s námi podívat na zatmění Slunce
+      </p>
+      <p>
+        Z určitého konkrétního místa na zemském povrchu lze úplné zatmění Slunce
+        pozorovat přibližně jednou asi za 360 let. Naposledy naši předci mohli
+        úplné zatmění Slunce z území dnešní České republiky vidět 12. května
+        1706. Příští úplné zatmění Slunce viditelné z území ČR nastane 7. října
+        2135.
+      </p>
+      <p>
+        Přímo v Evropě (Španělsko) budeme moci úplné zatmění Slunce spatřit 12.
+        srpna 2026. Ze západní části ČR uvidíme toto zatmění jako částečné, kdy
+        ve 20:11 hod SELČ těsně před západem Slunce při maximální fázi Měsíc
+        zakryje 88% Slunce.
+      </p>
+      <p>
+        2. srpna 2027 nastane úplné zatmění pozorovatelné například z Egypta.
+        Půjde o nejočekávanější zatmění, neboť pás zatmění prochází oblastmi s
+        nejmenším výskytem srážek na světě. V ČR uvidíme toto zatmění jako
+        částečné, kdy v 11:15 hod SELČ při maximální fázi Měsíc zakryje 51%
+        Slunce.
+      </p>
+    </div>
+  );
+}
+
+export default ZatmeniSlunce20brezna2015;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import ArticlesNav from "./ArticlesNav";
 import ChomutovVeVesmiru from "../articles_content/ChomutovVeVesmiru";
 import ProjektHvezdarnaFJGerstnera from "../articles_content/ProjektHvezdarnaFJGerstnera";
+import ZatmeniSlunce20brezna2015 from "../articles_content/ZatmeniSlunce20brezna2015";
 
 function Articles() {
   return (
@@ -17,6 +18,10 @@ function Articles() {
         <Route
           path="/articles/projekt-hvezdarna-f-j-gerstnera"
           component={ProjektHvezdarnaFJGerstnera}
+        />
+        <Route
+          path="/articles/zatmeni-slunce-20-brezna-2015"
+          component={ZatmeniSlunce20brezna2015}
         />
       </Switch>
     </div>
